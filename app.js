07ea7654b7a485558d1de24bfb3b0e70447555ac
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-31 01:54:20 +0300

    Template Engine EJS

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+
+app.use(express.static('public'))
+
+app.set("view engine","ejs");
 
 
-const personObject = {
-    Id : 1,
-    Name : "hasan",
-    Surname : "gursoy"
-}
 app.get('/',(req,res)=> {
-    res.send(personObject);
+    res.render('index');
+});
+app.get('/about',(req,res)=> {
+    res.render('about');
+});
+app.get('/add_post',(req,res)=> {
+    res.render('add_post');
 });
 
 
